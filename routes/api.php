4e37c243a42619e64f82311c8e6de4a47e32fd3a
Abrar <abrarahmed9423@gmail.com>
2023-11-30 20:50:22 -0600

    Routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('book', BookController::class)->except(['index', 'create', 'show', 'edit']);
 });
+
+// This route is create only for testing purposes
+Route::get('test/get-token/{user}', [TestController::class, 'generateToken']);
